167a7d2668a9390b5f8cc64918286534b7432abb
Destiny Rose <destinyrose2358@gmail.com>
2019-08-04 18:52:37 -0500

    Creates the queue class

--- W6D1/diy_adts.rb
+++ W6D1/diy_adts.rb
@@ -28,3 +28,37 @@ if __FILE__ == $PROGRAM_NAME
     puts stack.pop
     puts stack.peek
 end
+
+
+class Queue
+
+
+    def initialize
+        @queue = []
+    end
+
+    def enqueue(el)
+        @queue << el
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue.first
+    end
+
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+    queue = Queue.new
+    queue.enqueue("Sam")
+    p queue.peek
+    p queue.peek
+    queue.enqueue("Doug")
+    p queue.peek
+    p queue.dequeue
+    p queue.peek
+end
