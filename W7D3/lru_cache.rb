06c6e55556d83df2a05e7ff25be1bc0270098c75
Destiny Rose <destinyrose2358@gmail.com>
2019-08-14 00:09:03 -0500

    Finalizes lru cache homework

--- W7D3/lru_cache.rb
+++ W7D3/lru_cache.rb
@@ -25,4 +25,14 @@ class LRUCache
     # helper methods go here!
     attr_accessor :cache, :size
 
-  end
+end
+
+# to count takes O(n) time and O(1) space
+# to add an element takes O(n) time and O(1) space
+# to show the items take O(n) time and I'm not sure about space
+# this implementation of lru cache would be bad timewise
+# if you have a lot of elements compared to a typical implementation of
+# lru cache
+#to improve performance we would need to hash our elements, then store those elements
+# as a linked list so that each element is connected in the order of usage, but the hash just points
+# to the linked list object and the key is a name or some identifier for the object stored in the linked list node
