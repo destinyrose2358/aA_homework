fdb16751e189aee545a8d4770ee2377c8d477dd3
Destiny Rose <destinyrose2358@gmail.com>
2019-08-18 22:10:49 -0500

    Finalizes the plays-playwrights assignment

--- W8D1/plays-playwrights_orm/plays.rb
+++ W8D1/plays-playwrights_orm/plays.rb
@@ -38,9 +38,9 @@ class Play
       FROM
         plays
       JOIN
-        play_wrights ON plays.play_wright_id = play_wrights.id
+        playwrights ON plays.playwright_id = playwrights.id
       WHERE
-        play_wrights.name = ?
+        playwrights.name = ?
     SQL
     data.map { |datum| Play.new(datum) }
   end
@@ -74,4 +74,66 @@ class Play
         id = ?
     SQL
   end
+end
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |options| Playwright.new(options) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, self.name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+    data.map { |options| Playwright.new(options) }
+  end
+
+  def initialize(options)
+    @id = options["id"]
+    @name = options["name"]
+    @birth_year = options["birth_year"]
+  end
+
+  def create
+    raise "#{self} is already in the database" if id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+  end
+
+  def update
+    raise "#{self} is not in the database" unless id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    data = PlayDBConnection.instance.execute(<<-SQL, self.id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    data.map { |options| Play.new(options) }
+  end
+
 end
