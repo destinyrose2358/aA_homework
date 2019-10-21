ab2615e51eaa69285f7f8789723312623cf7902e
Destiny Rose <destinyrose2358@gmail.com>
2019-10-20 19:54:25 -0500

    Adds user model and registration route

--- mern_stack/app.js
+++ mern_stack/app.js
@@ -2,13 +2,23 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys.js").mongoURI;
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("AAAAAAHHHHHHH"));
-const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  console.log(res);
+  res.send("AAAAAAHHHHHHH");
+});
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
