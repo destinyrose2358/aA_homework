d03e395deff9a8654f27a91b40b65e01a2b891b6
Destiny Rose <destinyrose2358@gmail.com>
2019-11-04 00:39:13 -0600

    Finishes graphql homework

--- setup_graphql/index.js
+++ setup_graphql/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const expressGraphQL = require("express-graphql");
+const User = require("./models/User");
+const Post = require("./models/Post");
+const schema = require("./schema/schema");
 
-app.get("/", (req, res) => res.send("Hello World"));
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
